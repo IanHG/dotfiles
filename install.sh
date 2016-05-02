961f1925f01cfde1b4b590f5bfc0c9cd2df180e7
Ian H. Godtliebsen <ian@chem.au.dk>
2016-05-02 18:02:02 +0000

    improved install script to install vundle from github
    also added .gitignore to ignore vim/.vim

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+#######################################################
+# setup vim
+#######################################################
+# first clone Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git $PWD/vim/.vim
+# then setup symbolic links
+if [ -L ~/.vimrc ]; then rm ~/.vimrc; fi
 ln -s $PWD/vim/.vimrc ~/.vimrc
+if [ -L ~/.vim ]; then rm ~/.vim; fi
 ln -s $PWD/vim/.vim ~/.vim
