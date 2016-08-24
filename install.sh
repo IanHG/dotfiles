031ff37d24357342715f42c2c87f37890e70032d
Ian H. Godtliebsen <ian@chem.au.dk>
2016-08-24 16:38:50 +0200

    added bash aliases

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ if [ -L ~/.vimrc ]; then rm ~/.vimrc; fi
 ln -s $PWD/vim/vimrc ~/.vimrc
 if [ -L ~/.vim ]; then rm ~/.vim; fi
 ln -s $PWD/vim/vim ~/.vim
+if [ -L ~/.bash_aliases ]; then rm ~/.bash_aliases; fi
+ln -s $PWD/bash/bash_aliases ~/.bash_aliases
