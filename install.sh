81eedbcd26087eb5b12e5f833066c5edf14b68d9
Ian Heide Godtliebsen <ian@chem.au.dk>
2017-05-24 16:44:07 +0200

    added some git stuff, and updated bash_aliases

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #######################################################
-# setup vim
+# Vim
 #######################################################
 # first clone Vundle
 #git clone https://github.com/VundleVim/Vundle.vim.git $PWD/vim/.vim
@@ -10,5 +10,15 @@ if [ -L ~/.vimrc ]; then rm ~/.vimrc; fi
 ln -s $PWD/vim/vimrc ~/.vimrc
 if [ -L ~/.vim ]; then rm ~/.vim; fi
 ln -s $PWD/vim/vim ~/.vim
+
+#######################################################
+# Bash
+#######################################################
 if [ -L ~/.bash_aliases ]; then rm ~/.bash_aliases; fi
 ln -s $PWD/bash/bash_aliases ~/.bash_aliases
+
+#######################################################
+# Git
+#######################################################
+if [ -L ~/.gitconfig ]; then rm ~/.gitconfig; fi
+ln -s $PWD/git/gitconfig ~/.gitconfig
