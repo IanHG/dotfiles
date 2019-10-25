79898ca797531f7b7d68e4a9a917a49854ec2130
Ian Heide Godtliebsen <ian@chem.au.dk>
2019-10-25 15:21:36 +0200

    added file

---
+++ install_ubuntu_packages.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install neo-vim
+sudo apt-get install neovim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
