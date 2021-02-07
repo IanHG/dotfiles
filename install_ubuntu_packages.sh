3925e15986f85a1a878f31a7648f0381ddcdddb2
Ian Heide Godtliebsen <ian@chem.au.dk>
2021-02-07 21:10:17 +0100

    updated

--- install_ubuntu_packages.sh
+++ install_ubuntu_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# install neo-vim
+# Install neo-vim
 sudo apt-get install neovim
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
@@ -8,3 +8,6 @@ sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
+
+# Install i3
+sudo apt-get install i3
